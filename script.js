89bec234175fc5abfb95a4aa69e19fc2dafb2b09
unknown <jayshrikhirari18@gmail.com>
2024-04-27 17:20:47 +0530

    add new file

---
+++ script.js
@@ -0,0 +1,6 @@
+
+    let heading= document.querySelectorAll(".box-content")
+    heading[0].style.color="red";
+    heading[1].style.color="Green";
+    heading[2].style.color="blue";
+    heading[3].style.color="orange";
